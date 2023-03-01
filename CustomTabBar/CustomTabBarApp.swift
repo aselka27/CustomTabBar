e322f530d6c8d51632a82236bce2dba73f2e36ad
aselka27 <asjumalieva@gmail.com>
2023-03-01 15:11:37 +0600

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by саргашкаева on 1.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
